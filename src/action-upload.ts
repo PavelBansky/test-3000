32620d44fff14a453273bd834ebae0f7a255024c
Pavel Bansky <pavel@bansky.net>
2022-10-18 17:30:56 +0200

    little ui updates

--- src/action-upload.ts
+++ src/action-upload.ts
@@ -9,23 +9,24 @@ try {
 
   core.startGroup('List of env vars');
   core.info("GITHUB_WORKSPACE="+ env["GITHUB_WORKSPACE"]);
-  core.info("GITHUB_REPOSITORY"+ env["GITHUB_REPOSITORY"]);
-  core.info("GITHUB_REF"+ env["GITHUB_REF"]);
-  core.info("GITHUB_REF_NAME"+ env["GITHUB_REF_NAME"]);
-  core.info("GITHUB_SHA"+ env["GITHUB_SHA"]);
-  core.info("GITHUB_EVENT_NAME"+ env["GITHUB_EVENT_NAME"]);
-  core.info("RUNNER_OS"+ env["RUNNER_OS"]);
+  core.info("GITHUB_REPOSITORY="+ env["GITHUB_REPOSITORY"]);
+  core.info("GITHUB_REF="+ env["GITHUB_REF"]);
+  core.info("GITHUB_REF_NAME="+ env["GITHUB_REF_NAME"]);
+  core.info("GITHUB_SHA="+ env["GITHUB_SHA"]);
+  core.info("GITHUB_EVENT_NAME="+ env["GITHUB_EVENT_NAME"]);
+  core.info("RUNNER_OS="+ env["RUNNER_OS"]);
   core.endGroup();
 
   const time = (new Date()).toTimeString();
   core.setOutput("time", time); 
 
   core.startGroup('Context list');
-  core.info("Repo"+ github.context.repo);
-  core.info("Ref"+ github.context.ref);
-  core.info("Action"+ github.context.action);
-  core.info("Sha"+ github.context.sha);
-  core.info("Actor"+ github.context.actor);
+  core.info("Repo.Owner="+ github.context.repo.owner);
+  core.info("Repo.Repo="+ github.context.repo.repo);
+  core.info("Ref="+ github.context.ref);
+  core.info("Action="+ github.context.action);
+  core.info("Sha="+ github.context.sha);
+  core.info("Actor="+ github.context.actor);
   core.endGroup();
 
   core.startGroup('Payload dump');
